0e4860ce49f06e53045c0a8df5dd141395dfd0f7
Stevan <stevan.pavlicic@gmail.com>
2018-01-19 16:08:22 +0100

    commit before system upgrade

---
+++ src/main/java/org/akka/test/SimpleClusterApp.java
@@ -0,0 +1,27 @@
+package org.akka.test;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.cluster.Cluster;
+import akka.cluster.ClusterEvent.ClusterDomainEvent;
+
+public class SimpleClusterApp {
+
+	public static void main(String[] args) {
+		
+		// Override the configuration of the port
+	    // when specified as program argument
+		if (args.length > 0)
+			System.setProperty("akka.remote.netty.port", args[0]);
+		
+		// Create an Akka system
+		ActorSystem system = ActorSystem.create("ClusterSystem");
+		
+		// Create an actor that handles cluster domain events
+		ActorRef clusterListener = system.actorOf(new Props(null, SimpleClusterListener.class, null), "clusterListener");
+		
+		// Add subscription of cluster events
+	    Cluster.get(system).subscribe(clusterListener, ClusterDomainEvent.class);
+	}
+}
